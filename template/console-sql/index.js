ac416b7cda1b3a272eb04c4d9fd08c4af4ef6430
Intendant <contact@intendant.io>
2021-08-18 13:29:56 +0200

    Template

--- template/console-sql/index.js
+++
@@ -1,9 +0,0 @@
-const configuration = require('./intendant.json') 
-const console = require('@intendant/console-tracing')
-const core = require('@intendant/core')
-const connector = require('@intendant/sql-connector')
-const api = require('@intendant/core/gateways')
-
-api.initialisation(
-    new core(configuration,connector,console)
-)
