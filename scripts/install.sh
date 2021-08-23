ea61fcf5f4987b5a089e402cc7875a2ad2bd5dae
Intendant <contact@intendant.io>
2021-08-24 00:21:36 +0200

    Fix - install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -11,14 +11,14 @@ echo "
 echo "See more information on https://intendant.io";
 echo "";
 echo "> Download template.zip";
-curl -s  https://raw.githubusercontent.com/intendantio/intendant/main/template/console-sql.zip > template.zip;
+curl -s https://raw.githubusercontent.com/intendantio/intendant/main/template/console-sql.zip > template.zip;
 echo "> Unzip template.zip";
 unzip -o -qq template.zip;
 echo "> Remove template.zip";
 rm template.zip;
 echo "> Installation dependencies";
-npm --silent install;
+npm --silent install > "/dev/null";
 echo "> Installation successful";
-echo "> Edit the install.json file to finish the final configurations";
+echo "> Edit the intendant.json file to finish the final configurations";
 echo "> Press enter to finish";
 read varname
