e7fac831620874dbab0ee851a4b3c447625a6b61
Intendant <contact@intendant.io>
2021-08-26 19:09:35 +0200

    Fix - gitignore

---
+++ build/packages/@intendant/tracing/index.d.ts
@@ -0,0 +1,8 @@
+declare module '@intendant/tracing' {
+    class Log {
+        public static verbose(object: String, message: String): Boolean;
+        public static warning(object: String, message: String): Boolean;
+        public static error(object: String, message: String): Boolean;
+    }
+    export = Log
+}
---
+++ build/packages/@intendant/tracing/index.js
@@ -0,0 +1,1 @@
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.default=void 0;class Tracing{static verbose(){return!0}static warning(){return!0}static error(){return!0}}var _default=Tracing;exports.default=_default,module.exports=exports.default;
