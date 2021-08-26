18ed86894c7306c30d450e8484bf35840d215006
Intendant <contact@intendant.io>
2021-08-26 19:06:56 +0200

    Fix - Market support install

--- sources/core/controllers/Market.js
+++ sources/core/controllers/Market.js
@@ -45,23 +45,40 @@ class Market extends Controller {
             await extract(require('path').resolve('./') + "/.tmp-download.zip", { dir: require('path').resolve('./') + "/.intendant/" + pPackage })
             this.core.logger.verbose(Package.name,"Remove tmp file")
             fs.unlinkSync(require('path').resolve('./') + "/.tmp-download.zip")
+            
+            let tmpPackage = JSON.parse(fs.readFileSync(require('path').resolve('./') + "/package.json").toString())
+
+            let currentPackage = JSON.parse(fs.readFileSync(require('path').resolve('./') + "/.intendant/" + pPackage + "/package.json").toString())
+
+            for (let key in currentPackage.dependencies) {
+                    let dependencie = currentPackage.dependencies[key]
+                    let findInCurrent = false
+                    for (let tmpKey in tmpPackage.dependencies) {
+                        let tmpDependencie = tmpPackage.dependencies[tmpKey]
+                        if(key == tmpKey) {
+                            findInCurrent = tmpDependencie
+                        }
+                    }
+                    if(findInCurrent) {} else {
+                        this.core.logger.verbose(Package.name,"Add dependencie " + key + ":" + dependencie)
+                        tmpPackage.dependencies[key] = dependencie
+                    }
+                }
+            fs.writeFileSync(require('path').resolve('./') + "/package.json",JSON.stringify(tmpPackage), null, 4)
             await new Promise((resolve, reject) => {
                 npm.load(() => {
                     resolve()
                 })
             })
             await new Promise((resolve, reject) => {
-                npm.commands.install([require('path').resolve('./') + "/.intendant/" + pPackage + "/package.json","--silent"],() => {
+                npm.commands.install([],() => {
                     resolve()
                 })
             })
             this.core.logger.verbose(Package.name,"Restart configuration")
-            
             this.core.prepare()
-
             this.core.manager.module.restart()
             await this.core.manager.smartobject.restart()
-
             return {
                 code: 'ok',
                 message: "",
