b7649f8983896d60dc17a34ef8577ce017eb99fc
Intendant <contact@intendant.io>
2021-09-14 22:27:42 +0200

    Remove - useless tools

--- tools/exportEvaIcon.js
+++
@@ -1,6 +0,0 @@
-const fsextra = require('fs')
-let icons = []
-fsextra.readdirSync("../admin/public/ressource/icon").forEach(p => {
-    icons.push(p.replace(".svg",""))
-})
-fsextra.writeFileSync('../admin/public/ressource/icon.json',JSON.stringify(icons))
