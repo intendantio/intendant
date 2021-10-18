1dc1e8ca328e8c188f8f0efb83ff2472dceed045
Intendant <contact@intendant.io>
2021-10-19 01:20:04 +0200

    Fix - publish

--- scripts/market.js
+++ scripts/market.js
@@ -1,12 +1,6 @@
 const DraftLog = require('draftlog')
 const chalk = require('chalk')
 DraftLog(console)
-const moduleconf = require('./module.json')
-const fetch = require('node-fetch')
-const FormData = require('form-data')
-const fsextra = require('fs-extra')
-const fs = require('fs')
-const zipFolder = require('zip-a-folder')
 const { exec } = require('child_process')
 
 console.log()
@@ -24,7 +18,6 @@ exec("git reset", () => {
             update(chalk.white.bold.bgYellow(" >> ") + chalk(" git push "))
             exec("git push", () => {
                 update(chalk.white.bold.bgGreen(" >> ") + chalk(" Module publish ") + chalk.bold.green(" ✔"))
-                
             })
         })
     })
