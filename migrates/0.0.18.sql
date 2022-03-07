e5af51ce6d3eccd98f823589516cd9de24cc94c9
Intendant <intendant@clementmerlet.fr>
2022-03-08 00:21:53 +0100

    Delete useless file

--- migrates/0.0.18.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO "authorization_profile" ("id", "authorization", "profile") VALUES ('99', '77', '1');
-INSERT INTO "authorization" ("id", "reference", "method", "secure") VALUES ('77', '/essentials', 'GET', '1');
