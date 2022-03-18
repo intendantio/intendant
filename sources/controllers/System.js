ff255e84416a01df9b644cdc6513aa8e0f368126
Intendant <intendant@clementmerlet.fr>
2022-03-18 21:13:43 +0100

    Update System.js

--- sources/controllers/System.js
+++ sources/controllers/System.js
@@ -10,9 +10,9 @@ class System extends Controller {
 
     async upgrade() {
         try {
-            let result = await fetch("https://raw.githubusercontent.com/intendantio/intendant/main/template/package.json")
+            let result = await fetch("https://raw.githubusercontent.com/intendantio/intendant/main/releases/index.json")
             let resultJSON = await result.json()
-            let raw = resultJSON.dependencies["@intendant/core"]
+            let raw = resultJSON.url
             Tracing.verbose(Package.name, "Upgrade " + raw)
             await new Promise((resolve, reject) => {
                 exec("npm install " + raw + " -s --silent 2>&1", (e, std, ster) => {
